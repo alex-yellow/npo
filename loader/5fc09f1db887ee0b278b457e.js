6ec4162ccaae3d3253ab8f30f417b73021c54ffb
alex-yellow <72997598+alex-yellow@users.noreply.github.com>
2020-12-28 15:41:44 +0300

    Add files via upload

---
+++ loader/5fc09f1db887ee0b278b457e.js
@@ -0,0 +1,11 @@
+// @copyright MegaGroup © 2012 - 2018, AlexDesign
+// 2020-11-27 11:29:48
+// D: false 9 9
+(function (o,n,c) {
+  'use strict';
+  o[c] = o[c] || function() {(o[c].x=o[c].x||[]).push(arguments)};
+  var s = n.createElement('script'); s.type = 'text/javascript'; s.charset="utf-8";
+  s.async = true; s.src = "https://client.onicon.ru/visitor.js?v=4.4.83";
+  o[c]('init', {ver:'4.4.83',lang:'ru',staticClient: 'https://client.onicon.ru',siteId:'5fc09f1db887ee0b278b457e',server:'wss://n5.onicon.ru',initTime:new Date()});
+  n.getElementsByTagName('head')[0].appendChild(s);
+})(window, document, 'Onicon');
